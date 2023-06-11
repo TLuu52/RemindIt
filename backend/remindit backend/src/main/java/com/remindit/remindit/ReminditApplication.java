675e6334bb9e1ac329b96ce9371c9443797fd588
TLuu52 <luuthuan817@gmail.com>
2023-06-10 23:08:07 -0400

    added springboot backend into project
    still needs some tweaking but should be in the project now.

---
+++ backend/remindit backend/src/main/java/com/remindit/remindit/ReminditApplication.java
@@ -0,0 +1,13 @@
+package com.remindit.remindit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReminditApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReminditApplication.class, args);
+	}
+
+}

54678d0e27c186878b3555371b52cf2c74a34016
TLuu52 <luuthuan817@gmail.com>
2023-06-15 15:29:42 -0400

    set up signup to allow user data to be created. - still needs work
    setted up firestore and user authentication creation through the signup page but still won't upload the data to the firebase.

--- frontend/remindit-frontend/src/Front End Components/SignUp.jsx
+++ frontend/remindit-frontend/src/Front End Components/SignUp.jsx
@@ -5,7 +5,7 @@ import Logo from "./Utility/Logo"; // Importing the Logo component
 import { Link } from "react-router-dom";
 import React, { useState } from 'react';
 import { createUserWithEmailAndPassword } from 'firebase/auth';
-import { auth } from '../firebase';
+import { auth, firestore } from '../firebase';
 
 const Title = styled('h1')(({ theme }) => ({
     color: theme.palette.primary.contrastText,
@@ -58,39 +58,58 @@ const CustomLink = styled(Link)(({ theme }) => ({
 
 const SignUp = () => {
 
+    const [firstName, setFirstName] = useState("");
+    const [lastName, setLastName] = useState("");
     const [email, setEmail] = useState("");
     const [password, setPassword] = useState("");
 
     const signUp = (e) => {
         e.preventDefault();
         createUserWithEmailAndPassword(auth, email, password)
-          .then((userCredential) => {
-            console.log(userCredential);
-          })
-          .catch((error) => {
-            console.log(error);
-          });
-      }
-    
+            .then((userCredential) => {
+                // Save user information to Firestore
+                const user = userCredential.user;
+                const userRef = firestore.collection('users').doc(user.uid);
+
+                // Set the user data
+                userRef
+                    .set({
+                        firstName,
+                        lastName,
+                        email,
+                        password
+                    })
+                    .then(() => {
+                        console.log('User information saved to Firestore');
+                    })
+                    .catch((error) => {
+                        console.log('Error saving user information:', error);
+                    });
+            })
+            .catch((error) => {
+                console.log('Error creating user:', error);
+            });
+    };
+
 
     return (
-        <form onSubmit = {signUp}>
-        <Page>
-            <TopCorner>
-                <Logo />
-            </TopCorner>
-            <Cover>
-                <Title>Create a RemindIt Account</Title>
-                <Group>
-                    <CustomInput placeholder={'First Name'} size={'m'} style={{ margin: 'auto' }} />
-                    <CustomInput placeholder={'Last Name'} size={'m'} style={{ margin: 'auto' }} />
-                    <CustomInput placeholder={'Email'} size={'m'} style={{ margin: 'auto' }} value = {email} onChange ={(e) => setEmail(e.target.value)}/>
-                    <CustomInput placeholder={'Password'} size={'m'} style={{ margin: 'auto' }} type={'password'} value = {password} onChange ={(e) => setPassword(e.target.value)}/>
-                </Group>
-                <CustomButton text={'Create Account'} color={1} />
-                <BottomText>Already have an account? <CustomLink to='/'>Log in.</CustomLink></BottomText>
-            </Cover >
-        </Page>
+        <form onSubmit={signUp}>
+            <Page>
+                <TopCorner>
+                    <Logo />
+                </TopCorner>
+                <Cover>
+                    <Title>Create a RemindIt Account</Title>
+                    <Group>
+                        <CustomInput placeholder={'First Name'} size={'m'} style={{ margin: 'auto' }} value={firstName} onChange={(e) => setFirstName(e.target.value)} />
+                        <CustomInput placeholder={'Last Name'} size={'m'} style={{ margin: 'auto' }} value={lastName} onChange={(e) => setLastName(e.target.value)} />
+                        <CustomInput placeholder={'Email'} size={'m'} style={{ margin: 'auto' }} value={email} onChange={(e) => setEmail(e.target.value)} />
+                        <CustomInput placeholder={'Password'} size={'m'} style={{ margin: 'auto' }} type={'password'} value={password} onChange={(e) => setPassword(e.target.value)} />
+                    </Group>
+                    <CustomButton text={'Create Account'} color={1} />
+                    <BottomText>Already have an account? <CustomLink to='/'>Log in.</CustomLink></BottomText>
+                </Cover >
+            </Page>
         </form>
     )
 }
